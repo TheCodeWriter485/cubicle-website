3a56428f4f9e13e56917e55da00845189ebebf39
ultrasuperbeast <thecodewriter485@gmail.com>
2025-10-26 23:15:03 -0400

    MAJOR PROGRESS 1: TIER LIST
    The tier list is in a state I feel is presentable (a weekend of working on it). The tier list now populates based on a text file and pokeapi!

--- default project/script.js
+++ default project/script.js
@@ -1,32 +1,25 @@
-import Pokedex from 'pokedex-promise-v2';
-const P = new Pokedex();
-(async () => { // with Async/Await
-    try {
-        const golduckSpecies = await P.getPokemonSpeciesByName("golduck")
-        const frenchName = golduckSpecies.names.filter(pokeAPIName => pokeAPIName.language.name === 'fr')[0].name
-        console.log(frenchName)
-    } catch (error) {
-        throw error
-    }
-})()
+// pokeapi.js
 
-P.getPokemonByName(['eevee', 'ditto']) // with Promise
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log('There was an ERROR: ', error);
-  });
+// Import the PokeAPI wrapper from CDN context (already loaded in HTML)
+const P = new Pokedex.Pokedex();
 
-P.getPokemonByName(34, (response, error) => { // with callback
-    if(!error) {
-      console.log(response);
-    } else {
-      console.log(error)
-    }
-  });
-
-P.getResource(['/api/v2/pokemon/36', 'api/v2/berry/8', 'https://pokeapi.co/api/v2/ability/9/'])
-  .then((response) => {
-    console.log(response); // the getResource function accepts singles or arrays of URLs/paths
-  });
+/**
+ * Get Pokémon data by name from PokéAPI.
+ * @param {string} name - Pokémon name
+ * @returns {Promise<object|null>} - Pokémon data or null if not found
+ */
+async function getPokemonData(name) {
+  try {
+    const pokemon = await P.getPokemonByName(name.toLowerCase());
+    return {
+      name: pokemon.name,
+      id: pokemon.id,
+      types: pokemon.types.map(t => t.type.name),
+      abilities: pokemon.abilities.map(a => a.ability.name),
+      sprite: pokemon.sprites.front_default || pokemon.sprites.other["official-artwork"].front_default,
+    };
+  } catch (err) {
+    console.error(`Error fetching data for ${name}:`, err);
+    return null;
+  }
+}
