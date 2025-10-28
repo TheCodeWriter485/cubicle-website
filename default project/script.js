4c9ba3b48d9cf22e49e91d849f9751ed65070f0c
ultrasuperbeast <thecodewriter485@gmail.com>
2025-10-27 23:10:07 -0400

    MAJOR PROG 2! STATS
    Added hidden stat cards that show up when you click on a pokemon!
    
    Need to make them pretty but others tier list is basically done.

--- default project/script.js
+++ default project/script.js
@@ -14,9 +14,10 @@ async function getPokemonData(name) {
     return {
       name: pokemon.name,
       id: pokemon.id,
-      types: pokemon.types.map(t => t.type.name),
-      abilities: pokemon.abilities.map(a => a.ability.name),
+      types: pokemon.types.map(t => t.type.name).join(" "),
+      abilities: pokemon.abilities.map(a => a.ability.name).join("<br>"),
       sprite: pokemon.sprites.front_default || pokemon.sprites.other["official-artwork"].front_default,
+      stats: pokemon.stats.map(s => `${s.stat.name}: ${s.base_stat}`).join("<br>"),
     };
   } catch (err) {
     console.error(`Error fetching data for ${name}:`, err);
